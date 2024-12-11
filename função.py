67a67f7fd802758b42e398bb8238109e5c220c7d
ANA <ana.s.nascimento@aluno.senai.br>
2024-12-11 08:47:08 -0500

    para declarar todas função

---
+++ função.py
@@ -0,0 +1,14 @@
+#DECLARAR UMA FUNÇÃO
+def bom_dia():
+    print("bom dia!")
+
+
+def boa_noite():
+    print("BOA NOITE!")
+
+def boa_tarde():
+    print("BOA TARDE!")
+
+boa_noite()
+bom_dia()
+boa_tarde()
